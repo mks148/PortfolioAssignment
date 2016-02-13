03bbf59cdaac4e258b748c8e709c01c6c5a5d5e2
Manoj <mks148@gmail.com>
2016-02-12 22:17:34 -0500

    First Commit

---
+++ scripts/app.js
@@ -0,0 +1,33 @@
+"use strict";
+
+(function () {
+    
+
+
+
+    var firstparagraph;
+    firstparagraph = document.getElementById("intro");
+    firstparagraph.innerHTML = "Hi,My Name is Manoj Sharma and this is my startup web development company called <strong>craftelstudios</strong> portfolio . A Web Designer and  Developer from India.</br>I am CREATIVE & INNOVATIVE . I CRAFT YOUR IDEAS INTO BEAUTIFUL PIXELS. </br>I have very good skills in HTML5,Css,bootstrap and designing user interface designs in photoshop. </br> I am here in Georgain college to gain more knowledge about programming and startup with my own web design/development studio ";
+
+
+})();
+
+(function () {
+    var contactbutton;
+    contactbutton = document.getElementById("contact1");
+    contactbutton.innerHTML = "Contact Me ";
+
+})();
+
+
+(function () {
+    var footer;
+    footer = document.getElementById("foot");
+    footer.innerHTML = "Copyright©2016 CRAFTEL STUDIOS by Manoj Sharma";
+
+})();
+
+
+
+   
+  
---
+++ scripts/portfolio.js
@@ -0,0 +1,39 @@
+"use strict";
+
+(function () {
+    var work;
+    work = document.getElementById("head");
+    work.innerHTML = "MY WORKS";
+
+})();
+
+
+(function () {
+    var workone;
+    workone = document.getElementById("work1");
+    workone.innerHTML = "Mountain Logo";
+
+})();
+
+
+
+(function () {
+    var worktwo;
+    worktwo = document.getElementById("work2");
+    worktwo.innerHTML = "Bullion Logo";
+
+})();
+
+(function () {
+    var workthree;
+    workthree = document.getElementById("work3");
+    workthree.innerHTML = "Quotes Website Design";
+
+})();
+
+(function () {
+    var workfour;
+    workfour = document.getElementById("work4");
+    workfour.innerHTML = "Music App Design";
+
+})();
